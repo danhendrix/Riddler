3dfc3cbc4f2c0a9f888d29af6e444fe39f8f8c44
dhpdx <danhendrix@gmail.com>
2016-02-23 15:32:32 -0800

    Solves it

--- CarSales/car.py
+++ CarSales/car.py
@@ -1,61 +1,44 @@
 import random
 
-n = random.randint(1000,50001)
+def sale():
 
-cards = [n, n+100, n+200, n+300, n+400]
-random.shuffle(cards)
+	n = random.randint(1000,5000)
 
-options = [cards[0] + 100, cards[0] + 200, cards[0] + 300, cards[0] + 400, cards[0] - 100, cards[0] - 200, cards[0] - 300, cards[0] - 400]
+	cards = [n, n+100, n+200, n+300, n+400]
+	random.shuffle(cards)
+
+	options = [cards[0] + 100, cards[0] + 200, cards[0] + 300, cards[0] + 400, cards[0] - 100, cards[0] - 200, cards[0] - 300, cards[0] - 400]
 
 
+	options.remove(cards[1])
+	options = [x for x in options if x <= min(cards[0] + 400, cards[1] + 400) and x >= max(cards[0] - 400, cards[1] - 400)]
 
+	expected = sum(options)/len(options)
+	if expected > cards[1]:
+		return (cards[1] - n)
+	else:
+		options.remove(cards[2])
+		options = [x for x in options if x <= min(cards[0] + 400, cards[1] + 400, cards[2] + 400) and x >= max(cards[0] - 400, cards[1] - 400, cards[2] - 400)]
 
-print "Your first card is....{0}!".format(cards[0])
-choice = input("Do you want to take it? (Y)/(N)")
+		expected = sum(options)/len(options)
+		if expected > cards[2]:
+			return (cards[2] - n)
+		else:
+			options.remove(cards[3])
+			options = [x for x in options if x <= min(cards[0] + 400, cards[1] + 400, cards[2] + 400, cards[3] + 400) and x >= max(cards[0] - 400, cards[1] - 400, cards[2] - 400, cards[3] - 400)]
 
-if choice == "n" or "N":
-	print ("Ok..fine. Your next card is {0}").format(cards[1])
-else:
-	print "I think that means yes. Congratulations! By the way I would have taken {0}".format(n)
+			expected = sum(options)/len(options)
+			if expected > cards[3]:
+				return (cards[3] - n)
+			else:
+				return (cards[4] - n)
 
-options.remove(cards[1])
-options = [x for x in options if x <= min(cards[0] + 400, cards[1] + 400) and x >= max(cards[0] - 400, cards[1] - 400)]
-
-expected = sum(options)/len(options)
-
-print "Well, the expected value at this point is {0}".format(expected)
-choice = input("Do you want to take it? (Y)/(N)")
-
-if choice == "n" or "N":
-	print ("Ok..fine. Your next card is {0}").format(cards[2])
-else:
-	print "I think that means yes. Congratulations! By the way I would have taken {0}".format(n)
+count = 0
+results = []
+while count < 1000000:
+	results.append(sale())
+	count += 1
 	
-options.remove(cards[2])
-
-options = [x for x in options if x <= min(cards[0] + 400, cards[1] + 400, cards[2] + 400) and x >= max(cards[0] - 400, cards[1] - 400, cards[2] - 400)]
-
-expected = sum(options)/len(options)
-
-print "Well, the expected value at this point is {0}".format(expected)
-choice = input("Do you want to take it? (Y)/(N)")
-
-if choice == "n" or "N":
-	print ("Ok..fine. Your next card is {0}").format(cards[3])
-else:
-	print "I think that means yes. Congratulations! By the way I would have taken {0}".format(n)
-
-options.remove(cards[3])
-
-options = [x for x in options if x <= min(cards[0] + 400, cards[1] + 400, cards[2] + 400, cards[3] + 400) and x >= max(cards[0] - 400, cards[1] - 400, cards[2] - 400, cards[3] - 400)]
-
-expected = sum(options)/len(options)
-
-print "Well, the expected value at this point is {0}".format(expected)
-choice = input("Do you want to take it? (Y)/(N)")
-
-if choice == "n" or "N":
-	print "Well you held out for either the big prize or..not. Here's your price: ${0}".format(cards[4])
-else:
-	print "I think that means yes. Congratulations! By the way I would have taken {0}".format(n)
+print float(sum(results)/len(results))
 	
+
