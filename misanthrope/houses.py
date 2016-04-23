4de564c80a2772029588f65b77b3874bd392f7d8
dhpdx <dhendrix@gmail.com>
2016-04-23 11:05:39 -0700

    add full analysis, graph added going to 100

--- misanthrope/houses.py
+++ misanthrope/houses.py
@@ -1,4 +1,5 @@
 import random
+from bokeh.plotting import figure, output_file, show
 
 def movingIn(size):
 	open = range(size)
@@ -15,7 +16,28 @@ def movingIn(size):
 			open.remove(houseWanted)
 			houseVisits += 1
 			
-	return len(taken)
+	return float(len(taken))/size
 	
-print movingIn(100000)
+def test(number):
+	count = 0
+	results = []
+	while count < 1000:
+		results.append(movingIn(number))
+		count += 1
+	return float(sum(results))/1000
 	
+testResults = []
+numbers=range(101)
+
+for i in numbers[1:len(numbers)]:
+	testResults.append(test(i))
+	
+print testResults[99]
+
+output_file("lines.html", title="Home Buyers")
+
+p = figure(title="Misanthrope", x_axis_label="Number of Homes", y_axis_label='Percentage Occupied')
+
+p.line(numbers[1:len(numbers)],testResults)
+
+show(p)
